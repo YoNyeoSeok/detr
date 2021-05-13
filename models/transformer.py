c989f74a2af55bd40a1ab910ac349c6170460fae
YoNyeoSeok <yys8646@postech.ac.kr>
2021-05-14 03:44:55 +0900

    UPDATE first layer self-attn to use query embed

--- models/transformer.py
+++ models/transformer.py
@@ -52,9 +52,8 @@ class Transformer(nn.Module):
         query_embed = query_embed.unsqueeze(1).repeat(1, bs, 1)
         mask = mask.flatten(1)
 
-        tgt = torch.zeros_like(query_embed)
         memory = self.encoder(src, src_key_padding_mask=mask, pos=pos_embed)
-        hs = self.decoder(tgt, memory, memory_key_padding_mask=mask,
+        hs = self.decoder(query_embed, memory, memory_key_padding_mask=mask,
                           pos=pos_embed, query_pos=query_embed)
         return hs.transpose(1, 2), memory.permute(1, 2, 0).view(bs, c, h, w)
 
@@ -111,7 +110,7 @@ class TransformerDecoder(nn.Module):
                            memory_mask=memory_mask,
                            tgt_key_padding_mask=tgt_key_padding_mask,
                            memory_key_padding_mask=memory_key_padding_mask,
-                           pos=pos, query_pos=query_pos)
+                           pos=pos, query_pos=torch.zeros_like(query_pos) if i == 0 else query_pos)
             if self.return_intermediate:
                 intermediate.append(self.norm(output))
 
