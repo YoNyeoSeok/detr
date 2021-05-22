b110b5a680b0c9febb5aaa17f4c60d1609f98ae1
jhcho <cjh990910@gmail.com>
2021-05-23 03:36:16 +0900

    UPDATE swig resized256 setting

--- script/train.sh
+++ script/train.sh
@@ -17,12 +17,25 @@ git checkout bottomup_div3
 #     tee -a log/${NAME}.log
 
 # cluster 17 screen vgg16 AdamW 1
-NAME="bottomup_swig_div3_vgg16"
+# NAME="bottomup_swig_div3_vgg16"
+# OPTIM="AdamW"
+# rm -f log/${NAME}.log dist/${NAME}
+# rm -rf log/${NAME}
+# CUDA_VISIBLE_DEVICES="2,3" python -m torch.distributed.launch --nproc_per_node=2 --use_env \
+#     main.py --batch_size 16 --dataset_file swig --image_dir images_512 --epochs 50 --backbone vgg16 \
+#     --optimizer ${OPTIM} --lr 1e-4 --lr_backbone 1e-5 \
+#     --dist_url file://${PWD}/dist/${NAME} \
+#     --output_dir log/${NAME} |\
+#     while IFS= read -r line; do printf '%s %s\n' "$(date +%m:%d-%T)" "$line"; done |\
+#     tee -a log/${NAME}.log
+
+# cluster 3 swig resized 256 screen AdamW
+NAME="bottomup_swig_div3_resized256"
 OPTIM="AdamW"
 rm -f log/${NAME}.log dist/${NAME}
 rm -rf log/${NAME}
-CUDA_VISIBLE_DEVICES="2,3" python -m torch.distributed.launch --nproc_per_node=2 --use_env \
-    main.py --batch_size 16 --dataset_file swig --image_dir images_512 --epochs 50 --backbone vgg16 \
+CUDA_VISIBLE_DEVICES="0,1,2,3" python -m torch.distributed.launch --nproc_per_node=4 --use_env \
+    main.py --batch_size 32 --dataset_file swig --image_dir images_512 --epochs 50 --backbone resnet50 \
     --optimizer ${OPTIM} --lr 1e-4 --lr_backbone 1e-5 \
     --dist_url file://${PWD}/dist/${NAME} \
     --output_dir log/${NAME} |\
