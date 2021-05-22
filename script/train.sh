07d47438c6b14ef0178646e2f8a1f7fa2568ac3e
YoNyeoSeok <yys8646@postech.ac.kr>
2021-05-22 19:17:58 +0000

    UPDATE vgg16_bn setting

--- script/train.sh
+++ script/train.sh
@@ -4,30 +4,30 @@ git checkout bottomup_div3
 # train_bottomup
 # optimizer adamw
 # original image
-NAME="bottomup_swig_div3"
+# NAME="bottomup_swig_div3"
+# OPTIM="AdamW"
+# rm -f log/${NAME}.log dist/${NAME}
+# rm -rf log/${NAME}
+# CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --use_env \
+#     main.py --batch_size 32 --image_resize 512 --backbone resnet50 \
+#     --epochs 50 --optimizer ${OPTIM} --dataset_file swig --image_dir images_512 --lr 1e-4 --lr_backbone 1e-5 \
+#     --dist_url file://${PWD}/dist/${NAME} \
+#     --output_dir log/${NAME} |\
+#     while IFS= read -r line; do printf '%s %s\n' "$(date +%m:%d-%T)" "$line"; done |\
+#     tee -a log/${NAME}.log
+
+# cluster 17 screen vgg16_bn AdamW 1
+NAME="bottomup_swig_div3_vgg16bn"
 OPTIM="AdamW"
 rm -f log/${NAME}.log dist/${NAME}
 rm -rf log/${NAME}
-CUDA_VISIBLE_DEVICES="0,1" python -m torch.distributed.launch --nproc_per_node=2 --use_env \
-    main.py --batch_size 32 --image_resize 512 --backbone resnet50 \
+CUDA_VISIBLE_DEVICES="2,3" python -m torch.distributed.launch --nproc_per_node=2 --use_env \
+    main.py --batch_size 8 --image_resize 512 --backbone vgg16_bn \
     --epochs 50 --optimizer ${OPTIM} --dataset_file swig --image_dir images_512 --lr 1e-4 --lr_backbone 1e-5 \
     --dist_url file://${PWD}/dist/${NAME} \
     --output_dir log/${NAME} |\
     while IFS= read -r line; do printf '%s %s\n' "$(date +%m:%d-%T)" "$line"; done |\
     tee -a log/${NAME}.log
-
-# cluster 17 screen vgg16_bn AdamW 1
-# NAME="bottomup_swig_div3_vgg16"
-# OPTIM="AdamW"
-# rm -f log/${NAME}.log dist/${NAME}
-# rm -rf log/${NAME}
-# CUDA_VISIBLE_DEVICES="2,3" python -m torch.distributed.launch --nproc_per_node=2 --use_env \
-#     main.py --batch_size 16 --dataset_file swig --image_dir images_512 --epochs 50 --backbone vgg16_bn \
-#     --optimizer ${OPTIM} --lr 1e-4 --lr_backbone 1e-5 \
-#     --dist_url file://${PWD}/dist/${NAME} \
-#     --output_dir log/${NAME} |\
-#     while IFS= read -r line; do printf '%s %s\n' "$(date +%m:%d-%T)" "$line"; done |\
-#     tee -a log/${NAME}.log
 
 # cluster 3 swig resized 256 screen AdamW
 # NAME="bottomup_swig_div3_resized256"
